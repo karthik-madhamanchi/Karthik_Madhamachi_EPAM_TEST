03aae798290d3557c33f1edf52e6313d5d86e0f1
karthik-madhamanchi <karthikmadhamanchi2@gmail.com>
2017-09-15 15:49:25 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student st = new Student(01,"Karthik",new Date(1996,02,05),63.568);
+		Student st2 = new Student(02,"Kumar Karthik",new Date(1996,01,26),63.568);
+		Student st3 = new Student(03,"Ahmed Sohail",new Date(1996,05,06),63.568);
+		Student st4 = new Student(04,"Mahesh",new Date(1996,01,06),63.568);
+		
+		
+	}
+
+}
