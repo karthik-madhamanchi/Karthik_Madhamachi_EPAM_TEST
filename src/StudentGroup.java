b1c4b03712043086a661c28bd94344c5f2b05c52
karthik-madhamanchi <karthikmadhamanchi2@gmail.com>
2017-09-15 17:34:44 +0530

    commit 12

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -305,8 +305,10 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getStudentsByAge(int age) {
+		
 		Student[] s1=new Student[students.length];
 		int check=0;
+		
 		
 		for(int i=0;i<students.length;i++)
 		{
@@ -315,6 +317,7 @@ public class StudentGroup implements StudentArrayOperation {
 			s1[check++]=students[i];
 		}
 		return s1;
+	
 	}
 
 	@Override
@@ -357,4 +360,31 @@ public class StudentGroup implements StudentArrayOperation {
 			
 		return null;
 	}
+	public static int getAge(Date dateOfBirth) {
+    Calendar today = Calendar.getInstance();
+    Calendar birthDate = Calendar.getInstance();
+    birthDate.setTime(dateOfBirth);
+    if (birthDate.after(today)) {
+        throw new IllegalArgumentException("You don't exist yet");
+    }
+    int todayYear = today.get(Calendar.YEAR);
+    int birthDateYear = birthDate.get(Calendar.YEAR);
+    int todayDayOfYear = today.get(Calendar.DAY_OF_YEAR);
+    int birthDateDayOfYear = birthDate.get(Calendar.DAY_OF_YEAR);
+    int todayMonth = today.get(Calendar.MONTH);
+    int birthDateMonth = birthDate.get(Calendar.MONTH);
+    int todayDayOfMonth = today.get(Calendar.DAY_OF_MONTH);
+    int birthDateDayOfMonth = birthDate.get(Calendar.DAY_OF_MONTH);
+    int age = todayYear - birthDateYear;
+
+    
+    if ((birthDateDayOfYear - todayDayOfYear > 3) || (birthDateMonth > todayMonth)){
+        age--;
+    
+
+    } else if ((birthDateMonth == todayMonth) && (birthDateDayOfMonth > todayDayOfMonth)){
+        age--;
+    }
+    return age;
+}
 }
